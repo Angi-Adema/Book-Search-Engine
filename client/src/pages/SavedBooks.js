90538abb254d3400ac96e35f43b60e653489ec48
CoderGirl <angi.adema@gmail.com>
2023-01-25 21:35:16 -0700

    updated SavedBooks.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -7,7 +7,7 @@ import {
   Button,
 } from "react-bootstrap";
 
-import { getMe, deleteBook } from "../utils/API";
+// import { getMe, deleteBook } from "../utils/API"; Not needed any longer.
 import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 import { useMutation, useQuery } from "@apollo/react-hooks";
